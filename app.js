a9f1823d20b70d8b8aa1b3a6f70857deaac57009
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2021-10-01 22:58:14 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Taking user name as a input
+
+var userName = prompt("HEY , What's Your name ? ");
+
+//processing the value given by user
+
+var welcomingUser = "This Scripts is working Perfectly!! " + userName;
+
+//output
+
+alert(welcomingUser);
