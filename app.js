8f6de17636b337a8d562b428377416c21e43a7be
Akshat chaturvedi <akshatchaturvedi422@gmail.com>
2021-10-01 23:44:55 +0530

    Update : api added and also added some styling

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 
-var btnTranslate = document.querySelector("btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-var addEventListener = document.addEventListener("click",function translate() {
-    console.log("Translate button clicked!!");
-}); 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text="+text;
+}
+
+function errorHandler(error) {
+    console.log("error Occured",error);
+    alert("Something Wrong With server! Try again after Some Time");
+}
+
+btnTranslate.addEventListener("click",clickHandler);
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText)).then(response => 
+        response.json())
+        .then(json => { 
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}; 
